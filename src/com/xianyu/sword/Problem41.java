404acde2f5d71d15bae6e6d69fee8d8f447fbe45
zhouxianyu <zwgg.zhou@gmail.com>
2020-06-30 17:07:36 +0800

    [feat] problem 41 fix

--- src/com/xianyu/sword/Problem41.java
+++ src/com/xianyu/sword/Problem41.java
@@ -1,35 +1,54 @@
 package com.xianyu.sword;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * 如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，那么中位数就是所有数值排序之后位于中间的数值。如果从数据流中读出偶数个数值，那么中位数就是所有数值排序之后中间两个数的平均值。我们使用Insert()方法读取数据流，使用GetMedian()方法获取当前读取数据的中位数。
  */
 public class Problem41 {
 
-    ArrayList<Integer> list = new ArrayList<>();
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    private PriorityQueue<Integer> maxHeap = new PriorityQueue<>(new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o2 - o1;
+        }
+    });
 
     public void Insert(Integer num) {
-        int index = 0;
-        for (int i = 0; i < list.size(); i++) {
-            if (num > list.get(i)) {
-                index++;
-            }
+        if (num == null) {
+            return;
         }
-        list.add(index, num);
+        if (((maxHeap.size() + minHeap.size()) & 1) == 0) {
+            if (maxHeap.size() > 0 && maxHeap.peek() > num) {
+                maxHeap.offer(num);
+                num = maxHeap.poll();
+            }
+            minHeap.offer(num);
+        } else {
+            if (minHeap.size() > 0 && minHeap.peek() < num) {
+                minHeap.offer(num);
+                num = minHeap.poll();
+            }
+            maxHeap.offer(num);
+        }
     }
 
     public Double GetMedian() {
-        if (list.size() == 0){
+        if (minHeap.size() == 0 && maxHeap.size() == 0) {
             return null;
         }
-        if (list.size() % 2 == 0) {
-            double sum = list.get(list.size() / 2) + list.get(list.size() / 2 -1);
+        if (((minHeap.size() + maxHeap.size()) & 1) == 0) {
+            double sum = minHeap.peek() + maxHeap.peek();
             return sum / 2;
         } else {
-            return Double.valueOf(list.get(list.size() / 2));
+            double value = minHeap.peek()/2.0;
+            return Double.valueOf(value);
         }
     }
+
 
     public static void main(String[] args) {
         Problem41 problem41 = new Problem41();
