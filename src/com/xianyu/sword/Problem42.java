731eed25f6d70e0a6776e616f503288fd9e36271
zhouxianyu <zwgg.zhou@gmail.com>
2020-07-02 17:54:43 +0800

    [feat] problem 42 init

---
+++ src/com/xianyu/sword/Problem42.java
@@ -0,0 +1,20 @@
+package com.xianyu.sword;
+
+/**
+ * 连续子数组的最大和
+ */
+public class Problem42 {
+
+    public int FindGreatestSumOfSubArray(int[] array) {
+        if (array == null || array.length == 0) {
+            return 0;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
